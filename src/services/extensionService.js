04ee68e183e915de32f9ba7a30165e996870a0e4
Damjan15 <stamenovdamjan5@gmail.com>
2026-03-28 09:42:35 -0500

    feat: API services file created

---
+++ src/services/extensionService.js
@@ -0,0 +1,29 @@
+const BASE_URL = "http://localhost:3001/extensions";
+
+export const getExtensions = async () => {
+  const res = await fetch(BASE_URL);
+
+  if (!res.ok) throw new Error("Failed to fetch extensions");
+
+  return res.json();
+};
+
+export const toggleExtensions = async (id, isActive) => {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ isActive }),
+  });
+
+  if (!res.ok) throw new Error("Failed to update extensions");
+
+  return res.json();
+};
+
+export const removeExtensions = async (id) => {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error("Failed to delete extension");
+};
